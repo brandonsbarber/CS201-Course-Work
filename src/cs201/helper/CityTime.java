e516cab20fdd3e3f272f96db832a94fcacf4e014
Valakor <pohlmann@usc.edu>
2013-11-17 14:05:28 -0800

    Added comments and a constructor

--- src/cs201/helper/CityTime.java
+++ src/cs201/helper/CityTime.java
@@ -10,10 +10,30 @@ public class CityTime {
 	public int hour;
 	public int minute;
 	
+	/**
+	 * Creates a CityTime with the value 7:00AM Monday
+	 * 
+	 */
+	public CityTime() {
+		this.day = WeekDay.Monday;
+		this.hour = 7;
+		this.minute = 0;
+	}
+	
+	/**
+	 * Determines equality between this CityTime and another CityTime
+	 * @param other The CityTime being compared to this one
+	 * @return True if the two are the same
+	 */
 	public boolean equals(CityTime other) {
 		return this.day == other.day && this.hour == other.hour && this.minute == other.minute;
 	}
 	
+	/**
+	 * Enum for describing the days of the week
+	 * @author Matt Pohlmann
+	 *
+	 */
 	public enum WeekDay {
 		Sunday,
 		Monday,
