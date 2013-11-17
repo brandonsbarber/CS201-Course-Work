eb003a0ca7ed035c7b1135f3010e3cc239649903
Valakor <pohlmann@usc.edu>
2013-11-17 14:55:26 -0800

    Initial commit of class stubs for Restaurant Roles

---
+++ src/cs201/roles/restaurantRoles/RestaurantCustomerRole.java
@@ -0,0 +1,30 @@
+package cs201.roles.restaurantRoles;
+
+import cs201.agents.PersonAgent.Intention;
+import cs201.roles.Role;
+
+public class RestaurantCustomerRole extends Role {
+
+	public RestaurantCustomerRole() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void startInteraction(Intention intent) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected boolean pickAndExecuteAnAction() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void closingTime() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
