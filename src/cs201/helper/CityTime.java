80340e6cb5af729040c9462b0e890836537c3d88
Valakor <pohlmann@usc.edu>
2013-11-17 13:48:43 -0800

    Implemented CityTime class

--- src/cs201/helper/CityTime.java
+++ src/cs201/helper/CityTime.java
@@ -1,5 +1,26 @@
 package cs201.helper;
 
+/**
+ * General-purpose class for determining the time in SimCity201
+ * @author Matt Pohlmann
+ *
+ */
 public class CityTime {
-
+	public WeekDay day;
+	public int hour;
+	public int minute;
+	
+	public boolean equals(CityTime other) {
+		return this.day == other.day && this.hour == other.hour && this.minute == other.minute;
+	}
+	
+	public enum WeekDay {
+		Sunday,
+		Monday,
+		Tuesday,
+		Wednesday,
+		Thursday,
+		Friday,
+		Saturday
+	}
 }
