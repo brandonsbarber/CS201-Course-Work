e8a975cbf55925a0d2d7f59c7e4ffb3b2dd4dca6
Valakor <pohlmann@usc.edu>
2013-11-17 14:37:04 -0800

    Clarified what Main actually is

--- src/cs201/Main.java
+++ src/cs201/Main.java
@@ -1,10 +1,15 @@
 package cs201;
 
+/**
+ * Main entry point for the program. Creates the main window and that's it
+ * @author Matt Pohlmann
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		// Will create the main SimCity201 window here
 	}
 
 }
