a0474b11a7ccac78a613c98e3368d642e22a232b
Brandon Barber <brandonsbarber@gmail.com>
2013-11-20 10:14:54 -0800

    Added Bus and Truck agents

---
+++ src/cs201/agents/transit/BusAgent.java
@@ -0,0 +1,37 @@
+package cs201.agents.transit;
+
+import cs201.interfaces.transit.Bus;
+import cs201.interfaces.transit.Passenger;
+
+public class BusAgent extends VehicleAgent implements Bus
+{
+	@Override
+	public void msgLeaving(Passenger p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void msgStaying(Passenger p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void msgDoneBoarding(Passenger p) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void msgNotBoarding(Passenger p) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	protected boolean pickAndExecuteAnAction() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
---
+++ src/cs201/agents/transit/TruckAgent.java
@@ -0,0 +1,25 @@
+package cs201.agents.transit;
+
+import java.util.Map;
+
+import cs201.interfaces.transit.Truck;
+import cs201.structures.Structure;
+
+public class TruckAgent extends VehicleAgent implements Truck
+{
+
+	@Override
+	public void msgMakeDeliveryRun(Map<String, Integer> inventory, Structure destination)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected boolean pickAndExecuteAnAction()
+	{
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
