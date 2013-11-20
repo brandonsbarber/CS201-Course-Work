5ba4a191c9d6d85d6ae64d2916897afd1ee20b2f
Brandon Barber <brandonsbarber@gmail.com>
2013-11-20 10:32:59 -0800

    Working on BusAgent

--- src/cs201/agents/transit/BusAgent.java
+++ src/cs201/agents/transit/BusAgent.java
@@ -1,32 +1,54 @@
 package cs201.agents.transit;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.Semaphore;
 
+import cs201.helper.transit.BusRoute;
 import cs201.interfaces.transit.Bus;
 import cs201.interfaces.transit.Passenger;
+import cs201.structures.transit.BusStop;
 
 public class BusAgent extends VehicleAgent implements Bus
 {
 	List<Passenger> passengers;
+	List<Passenger> justBoarded;
 	
+	BusRoute route;
 	
+	Semaphore sem;
+	
+	public BusAgent(BusRoute route)
+	{
+		passengers = Collections.synchronizedList(new ArrayList<Passenger>());
+		this.route = route;
+		sem = new Semaphore(0);
+	}
+	
+	public BusRoute getRoute()
+	{
+		return route;
+	}
 	
 	@Override
-	public void msgLeaving(Passenger p) {
-		// TODO Auto-generated method stub
-		
+	public void msgLeaving(Passenger p)
+	{
+		passengers.remove(p);
+		sem.release();
 	}
 
 	@Override
-	public void msgStaying(Passenger p) {
-		// TODO Auto-generated method stub
-		
+	public void msgStaying(Passenger p)
+	{
+		sem.release();
 	}
 
 	@Override
-	public void msgDoneBoarding(Passenger p) {
-		// TODO Auto-generated method stub
-		
+	public void msgDoneBoarding(Passenger p)
+	{
+		passengers.add(p);
+		justBoarded.add(p);
 	}
 
 	@Override
--- src/cs201/structures/transit/BusStop.java
+++ src/cs201/structures/transit/BusStop.java
@@ -1,6 +1,60 @@
 package cs201.structures.transit;
 
-public class BusStop
-{
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+import cs201.agents.PersonAgent.Intention;
+import cs201.interfaces.transit.Bus;
+import cs201.interfaces.transit.Passenger;
+import cs201.roles.Role;
+import cs201.structures.Structure;
+
+public class BusStop extends Structure
+{
+	List<Passenger> waitingPassengers;
+	
+	List<Passenger> addRequests;
+	
+	Bus parkedBus;
+	
+	public BusStop(int x, int y, int width, int height, int id)
+	{
+		super(x, y, width, height, id);
+		waitingPassengers = Collections.synchronizedList(new ArrayList<Passenger>());
+		addRequests = new ArrayList<Passenger>();
+	}
+
+	@Override
+	public Role getRole(Intention role)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public List<Passenger> getPassengerList(Bus bus)
+	{
+		if(parkedBus == null)
+		{
+			parkedBus = bus;
+			return waitingPassengers;
+		}
+		return null;
+	}
+	
+	public void addPassenger(Passenger p)
+	{
+		addRequests.add(p);
+	}
+	
+	public void removePassengers(Bus bus, List<Passenger> passengers)
+	{
+		if(bus == parkedBus)
+		{
+			waitingPassengers.removeAll(passengers);
+			waitingPassengers.addAll(addRequests);
+			addRequests.clear();
+			parkedBus = null;
+		}
+	}
 }
