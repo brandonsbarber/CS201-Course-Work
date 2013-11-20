0c6e1e562f7ac8a5c878aa569014e33322ba79c1
Brandon Barber <brandonsbarber@gmail.com>
2013-11-20 01:50:57 -0800

    Updated Passenger interface

--- src/cs201/interfaces/transit/Passenger.java
+++ src/cs201/interfaces/transit/Passenger.java
@@ -1,5 +1,12 @@
 package cs201.interfaces.transit;
 
-public interface Passenger {
+import cs201.structures.Structure;
 
+public interface Passenger
+{
+	public void msgGoTo (Structure s);
+	
+	public void msgPleaseBoard (Vehicle v);
+	
+	public void msgReachedDestination (Structure s);
 }
