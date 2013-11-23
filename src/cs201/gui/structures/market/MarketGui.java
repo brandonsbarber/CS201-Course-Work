ffbe833aea28f4a7a8667773e7cfcc22948146c3
Ben Doherty <bdoherty@usc.edu>
2013-11-22 21:47:20 -0800

    Added MarketGui

---
+++ src/cs201/gui/structures/market/MarketGui.java
@@ -0,0 +1,21 @@
+package cs201.gui.structures.market;
+
+import java.awt.geom.Rectangle2D;
+
+import cs201.gui.SimCity201;
+import cs201.gui.StructurePanel;
+import cs201.roles.Role;
+
+public class MarketGui extends StructurePanel {
+
+	public MarketGui(Rectangle2D r, int i, SimCity201 sc) {
+		super(r, i, sc);
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setPersonEnabled(Role role) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
