1e3a51fcf45d344a3ee086ee67028134640afa3e
Valakor <pohlmann@usc.edu>
2013-11-23 18:38:40 -0800

    Testing PersonAgent still

--- src/cs201/gui/SimCity201.java
+++ src/cs201/gui/SimCity201.java
@@ -12,6 +12,7 @@ import cs201.agents.PersonAgent;
 import cs201.agents.PersonAgent.Intention;
 import cs201.gui.structures.restaurant.RestaurantGuiMatt;
 import cs201.helper.CityDirectory;
+import cs201.structures.residence.Residence;
 import cs201.structures.restaurant.RestaurantMatt;
 
 public class SimCity201 extends JFrame {
@@ -54,11 +55,15 @@ public class SimCity201 extends JFrame {
 		CityDirectory.getInstance().addRestaurant(r2);
 		cityPanel.addStructure(r2);
 		
+		Residence r3 = new Residence(40, 160, 40, 40, 2);
+		CityDirectory.getInstance().addResidence(r3);
+		cityPanel.addStructure(r3);
+		
 		add(BorderLayout.NORTH, cityPanel);
 		add(BorderLayout.SOUTH, buildingPanels);
 		
 		PersonAgent p = new PersonAgent("Matt");
-		p.setupPerson(CityDirectory.getInstance().getTime(), null, r2, Intention.RestaurantHost, null, null);
+		p.setupPerson(CityDirectory.getInstance().getTime(), r3, r2, Intention.RestaurantHost, r3, null);
 		p.startThread();
 		CityDirectory.getInstance().addPerson(p);
 		CityDirectory.getInstance().startTime();
