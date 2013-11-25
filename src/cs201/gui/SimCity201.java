b5b80d345abe1f6ac761538ebbd975ac18a71604
Brandon Barber <brandonsbarber@gmail.com>
2013-11-24 20:39:21 -0800

    Tweaked SimCity and added initial settings panel

--- src/cs201/gui/SimCity201.java
+++ src/cs201/gui/SimCity201.java
@@ -12,47 +12,64 @@ import javax.swing.JScrollPane;
 import cs201.helper.CityDirectory;
 
 public class SimCity201 extends JFrame {
-	private final int SIZEX = 1200;
+	private final int SIZEX = 1500;
 	private final int SIZEY	= 700;
 	
 	CityPanel cityPanel;
 	JPanel buildingPanels;
 	CardLayout cardLayout;
 	
+	SettingsPanel settingsPanel;
+	
 	public SimCity201() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 		setSize(SIZEX, SIZEY);
 		
+		JPanel guiPanel = new JPanel();
+		
 		setLayout(new BorderLayout());
 		
+		guiPanel.setLayout(new BorderLayout());
+		
 		cityPanel = new CityPanel();
-		cityPanel.setPreferredSize(new Dimension(SIZEX / 2, SIZEY));
-		cityPanel.setMaximumSize(new Dimension(SIZEX / 2, SIZEY));
-		cityPanel.setMinimumSize(new Dimension(SIZEX / 2, SIZEY));
+		cityPanel.setPreferredSize(new Dimension(SIZEX / 3, SIZEY));
+		cityPanel.setMaximumSize(new Dimension(SIZEX / 3, SIZEY));
+		cityPanel.setMinimumSize(new Dimension(SIZEX / 3, SIZEY));
 		
 		cardLayout = new CardLayout();
 		
 		buildingPanels = new JPanel();
 		buildingPanels.setLayout(cardLayout);
-		buildingPanels.setMinimumSize(new Dimension(SIZEX / 2, SIZEY));
-		buildingPanels.setMaximumSize(new Dimension(SIZEX / 2, SIZEY));
-		buildingPanels.setPreferredSize(new Dimension(SIZEX / 2, SIZEY));
+		buildingPanels.setMinimumSize(new Dimension(SIZEX / 3, SIZEY));
+		buildingPanels.setMaximumSize(new Dimension(SIZEX / 3, SIZEY));
+		buildingPanels.setPreferredSize(new Dimension(SIZEX / 3, SIZEY));
 		buildingPanels.setBackground(Color.YELLOW);
 
 		// Create initial buildings here and add them to cityPanel and buildingPanels
 		
 		JScrollPane cityScrollPane = new JScrollPane(cityPanel);
 		
-		cityScrollPane.setMinimumSize(new Dimension(SIZEX / 2, SIZEY));
-		cityScrollPane.setMaximumSize(new Dimension(SIZEX / 2, SIZEY));
-		cityScrollPane.setPreferredSize(new Dimension(SIZEX / 2, SIZEY));
+		cityScrollPane.setMinimumSize(new Dimension(SIZEX / 3, SIZEY));
+		cityScrollPane.setMaximumSize(new Dimension(SIZEX / 3, SIZEY));
+		cityScrollPane.setPreferredSize(new Dimension(SIZEX / 3, SIZEY));
 		
 		cityScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
 		cityScrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
 		
-		add(BorderLayout.WEST, cityScrollPane);
-		add(BorderLayout.EAST, buildingPanels);
+		guiPanel.add(BorderLayout.WEST, cityScrollPane);
+		guiPanel.add(BorderLayout.EAST, buildingPanels);
+		
+		settingsPanel = new SettingsPanel();
+		
+		settingsPanel.setMinimumSize(new Dimension(SIZEX / 3, SIZEY));
+		settingsPanel.setMaximumSize(new Dimension(SIZEX / 3, SIZEY));
+		settingsPanel.setPreferredSize(new Dimension(SIZEX / 3, SIZEY));
+		
+		add(settingsPanel,BorderLayout.WEST);
+		add(guiPanel);
+		
+		
 		
 		pack();
 		CityDirectory.getInstance().startTime();
