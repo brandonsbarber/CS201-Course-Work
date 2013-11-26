4fade1b10a8a566f5510c181ceea46d2a1872cfc
Skyler Lloyd <skylerll@usc.edu>
2013-11-25 20:11:09 -0800

    Added some residence graphical elements

--- src/cs201/gui/structures/residence/ResidenceAnimationPanel.java
+++ src/cs201/gui/structures/residence/ResidenceAnimationPanel.java
@@ -1,5 +1,7 @@
 package cs201.gui.structures.residence;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
@@ -17,16 +19,17 @@ public class ResidenceAnimationPanel extends StructurePanel {
 	
 	private final int bedWidth = 70;
 	private final int bedHeight = 30;
-	private final int bedX = 100;
-	private final int bedY = 100;
+	private final int bedX = 250;
+	private final int bedY = 430;
 	
+	private final int entranceY = 250;
 	
 	private final int fridgeWidth = 30;
-	private final int fridgeX = 300;
-	private final int fridgeY = 300;
-	
-	private final int tableWidth = 70;
-	private final int tableHeight = 40;
+	private final int fridgeX = 250;
+	private final int fridgeY = 10;
+
+	private final int tableWidth = 40;
+	private final int tableHeight = 70;
 	private final int tableX = 70;
 	private final int tableY = 30;
 	
@@ -37,17 +40,28 @@ public class ResidenceAnimationPanel extends StructurePanel {
 		super(i, sc);
 		// TODO Auto-generated constructor stub
 		setSize(WINDOWX, WINDOWY);
+		setMaximumSize(new Dimension(WINDOWX, WINDOWY));
+		setMinimumSize(new Dimension(WINDOWX, WINDOWY));
 		setVisible(true);
 	}
 	
 	public void paintComponent(Graphics g) {
+		
 		Graphics2D g2 = (Graphics2D)g;
-	    for(Gui gui : guis) {
-	        if (gui.isPresent()) {
-	            gui.updatePosition();
-	            gui.draw(g2);
-	        }
-	    }
+		
+		g2.setColor(getBackground());
+		g2.fillRect(0, 0, WINDOWX, WINDOWY); //clear the screen
+		
+		g2.setColor(Color.GRAY);
+		g2.fillRect(fridgeX, fridgeY, fridgeWidth, fridgeWidth); // draws refrigerator
+		
+		g2.setColor(Color.GREEN);
+		g2.fillRect(tableX, tableY, tableWidth, tableHeight); // draws dining table
+		
+		g2.setColor(Color.BLUE);
+		g2.fillRect(bedX, bedY, bedWidth, bedHeight); // draws bed
+		
+		super.paintComponent(g);
 
 	}
 	
