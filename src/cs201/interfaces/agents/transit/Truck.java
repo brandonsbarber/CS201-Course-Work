01cd83900c742e715d12e50d551b461eec484d79
Brandon Barber <brandonsbarber@gmail.com>
2013-11-26 22:08:48 -0800

    Commented interfaces

--- src/cs201/interfaces/agents/transit/Truck.java
+++ src/cs201/interfaces/agents/transit/Truck.java
@@ -5,7 +5,18 @@ import java.util.List;
 import cs201.roles.marketRoles.MarketManagerRole.ItemRequest;
 import cs201.structures.Structure;
 
+/**
+ * 
+ * @author Brandon
+ *
+ */
 public interface Truck extends Vehicle
 {
+	/**
+	 * Sends truck to make delivery
+	 * @param inventory what to deliver
+	 * @param destination where to deliver
+	 * @param price what to charge
+	 */
 	public void msgMakeDeliveryRun (List<ItemRequest> inventory, Structure destination, double price);
 }
