266065990d06fb7d2cdeca9f1379be0125c6e37c
Brandon Barber <brandonsbarber@gmail.com>
2013-12-03 11:38:09 -0800

    Added class for Bus Stop Gui

---
+++ src/cs201/gui/structures/transit/BusStopGui.java
@@ -0,0 +1,15 @@
+package cs201.gui.structures.transit;
+
+import cs201.gui.SimCity201;
+import cs201.gui.StructurePanel;
+
+public class BusStopGui extends StructurePanel
+{
+
+	public BusStopGui(int i, SimCity201 sc)
+	{
+		super(i, sc);
+		
+	}
+
+}
