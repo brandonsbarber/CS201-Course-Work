2c3146aab42880fad2574d91eb4b2e5473bb69d0
Brandon Barber <brandonsbarber@gmail.com>
2013-12-02 23:31:53 -0800

    Fixed package issue with bank Gui

---
+++ src/cs201/gui/roles/bank/BankGuardGui.java
@@ -0,0 +1,34 @@
+package cs201.gui.roles.bank;
+
+import java.awt.Graphics2D;
+
+import cs201.gui.Gui;
+
+public class BankGuardGui implements Gui
+{
+
+	@Override
+	public void updatePosition() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void draw(Graphics2D g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isPresent() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setPresent(boolean present) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
