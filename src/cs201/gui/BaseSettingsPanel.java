701cd23b49c15ebe4bd3443e7788995d09e48185
Matthew Pohlmann <pohlmann@usc.edu>
2013-12-06 17:24:17 -0800

    Trace panel now pops up when you click the trace panel button

--- src/cs201/gui/BaseSettingsPanel.java
+++ src/cs201/gui/BaseSettingsPanel.java
@@ -1,25 +1,41 @@
 package cs201.gui;
 
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
-import javax.swing.JLabel;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 
 import cs201.helper.Constants;
+import cs201.trace.TracePanel;
 
 public class BaseSettingsPanel extends JPanel implements ActionListener
 {
+	private final int TRACEX = 800;
+	private final int TRACEY = 300;
+	
 	private JButton traceButton;
 	private JCheckBox debugMode;
+	private JFrame traceFrame;
+	private TracePanel tracePanel;
 	
 	public BaseSettingsPanel()
 	{
 		setLayout(new FlowLayout());
+		
+		tracePanel = new TracePanel();
+		tracePanel.setPreferredSize(new Dimension(TRACEX, TRACEY));
+		traceFrame = new JFrame();
+		traceFrame.setResizable(false);
+		traceFrame.add(tracePanel);
+		traceFrame.pack();
+		traceFrame.setVisible(false);
+		
 		traceButton = new JButton("Show Trace Panel");
 		traceButton.addActionListener(this);
 		add(traceButton);
@@ -38,6 +54,9 @@ public class BaseSettingsPanel extends JPanel implements ActionListener
 		if(e.getSource() == debugMode)
 		{
 			Constants.DEBUG_MODE = debugMode.isSelected();
+		}
+		if (e.getSource() == traceButton) {
+			traceFrame.setVisible(!traceFrame.isVisible());
 		}
 	}
 }
