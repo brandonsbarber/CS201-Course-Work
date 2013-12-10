b3197a02125bf9b13e211cf5053f72449f41b6a7
Skyler Lloyd <skylerll@usc.edu>
2013-12-10 13:07:11 -0800

    Changed window height back to 700 for my testing.

--- src/cs201/gui/SimCity201.java
+++ src/cs201/gui/SimCity201.java
@@ -54,7 +54,7 @@ import cs201.structures.transit.BusStop;
 @SuppressWarnings("serial")
 public class SimCity201 extends JFrame {
 	public static final int SIZEX = 1200;
-	public static final int SIZEY = 800;
+	public static final int SIZEY = 700;
 	
 	CityPanel cityPanel;
 	JPanel buildingPanels;
